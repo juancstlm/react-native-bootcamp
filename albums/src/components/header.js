39a32573132de9c1fd1000b0865a8076cacbde4e
Juan Castillo <juancstl@protonmail.com>
2018-09-03 20:39:42 -0700

    Style header

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 const Header = () => {
-    return <Text>Albums</Text>
+    const {textStyle, viewStyle} = styles;
+    return(
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>);
 };
 
+const styles = {
+    viewStyle: {
+      backgroundColor: '#195f71',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        elevation: 2,
+        position: 'relative',
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#FFFFFF'
+    }
+};
 export default Header;
