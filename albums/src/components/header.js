8647fa4fd2fd91077378ed32f52bd0c878fe57c3
Juan Castillo <juancstl@protonmail.com>
2018-09-07 10:54:59 -0700

    Set status bar color

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StatusBar} from 'react-native';
 
 const Header = (props) => {
     const {textStyle, viewStyle} = styles;
     return(
         <View style={viewStyle}>
+            <StatusBar
+                barStyle="light-content"
+            />
             <Text style={textStyle}>{props.headerText}</Text>
         </View>);
 };
 
 const styles = {
     viewStyle: {
-      backgroundColor: '#195f71',
+      backgroundColor: '#2F3A49',
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
