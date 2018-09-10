4231a51a36017d7b7a832ccfb8139c6b82220acc
Juan Castillo <juancstl@protonmail.com>
2018-09-09 23:28:59 -0700

    Add AlbumDetail component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
 import axios from 'axios'
+import AlbumDetail from './AlbumDetail'
 
 export default class AlbumList extends React.Component {
+    state = { albums: []};
+
     componentWillMount(){
         axios.get('https://rallycoding.herokuapp.com./api/music_albums')
-            .then(response => console.log(response))
+            .then(response => this.setState({albums: response.data}))
     }
+
+    renderAlbums () {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album}/>)
+    }
+
     render() {
+        console.log(this.state)
+
         return (
             <View>
-                <Text>Album List!</Text>
+                {this.renderAlbums()}
             </View>
         )
     }
